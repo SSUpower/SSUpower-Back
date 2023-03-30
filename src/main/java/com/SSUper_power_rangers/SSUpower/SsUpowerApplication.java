1bb71158771d4cace8f98d3f47d352903101af7e
byeoneunyoung <ebyeon147@soongsil.ac.kr>
2023-03-30 17:11:10 +0900

    school_db

---
+++ src/main/java/com/SSUper_power_rangers/SSUpower/SsUpowerApplication.java
@@ -0,0 +1,13 @@
+package com.SSUper_power_rangers.SSUpower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsUpowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsUpowerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SSUper_power_rangers/SSUpower/SsUpowerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SSUper_power_rangers.SSUpower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsUpowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
