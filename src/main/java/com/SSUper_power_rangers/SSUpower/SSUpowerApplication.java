afe6ed9fe700b552fdedc7ea1c61dcdab0d2f49d
byeoneunyoung <ebyeon147@soongsil.ac.kr>
2023-04-03 18:54:00 +0900

    spring boot init

--- src/main/java/com/SSUper_power_rangers/SSUpower/SSUpowerApplication.java
+++ src/main/java/com/SSUper_power_rangers/SSUpower/SSUpowerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SsUpowerApplication {
+public class SSUpowerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SsUpowerApplication.class, args);
+		SpringApplication.run(SSUpowerApplication.class, args);
 	}
 
 }
