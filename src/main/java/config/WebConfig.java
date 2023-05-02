756ee13607502105a5470601b072a50e556a3f7a
byeoneunyoung <ebyeon147@soongsil.ac.kr>
2023-05-02 22:44:11 +0900

    web configuration modification

--- src/main/java/config/WebConfig.java
+++ src/main/java/config/WebConfig.java
@@ -8,7 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000");
+        registry.addMapping("/map")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET");
     }
 }
