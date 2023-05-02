872679775a881ca570d9af96e662dd041a27fb0a
byeoneunyoung <ebyeon147@soongsil.ac.kr>
2023-05-02 17:10:01 +0900

    timetable, building join

---
+++ src/main/java/SSUpower/demo/SimpleMessageDAO.java
@@ -0,0 +1,33 @@
+package SSUpower.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class SimpleMessageDAO {
+    @Autowired
+    JdbcTemplate jt;
+
+    public List<Map<String, ?>> selectAll() {
+
+        return jt.query("SELECT * FROM school_db.timetable, school_db.building where school_db.timetable.buildingID = school_db.building._id;", (rs, rowNum) -> {
+            Map<String, Object> mss = new HashMap<>();
+            mss.put("buildingId", rs.getInt(1));
+            mss.put("classID", rs.getInt(2));
+            mss.put("object", rs.getString(3));
+            mss.put("start", rs.getInt(4));
+            mss.put("end", rs.getInt(5));
+            mss.put("day", rs.getString(6));
+            mss.put("_id", rs.getInt(7));
+            mss.put("building_name", rs.getString(8));
+            mss.put("latitude", rs.getDouble(9));
+            mss.put("longitude", rs.getDouble(10));
+            return mss;
+        });
+    }
+}
---
+++ src/main/java/SSUpower/demo/TestController.java
@@ -0,0 +1,21 @@
+package SSUpower.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+@RestController
+public class TestController {
+    @Autowired
+    SimpleMessageDAO smd;
+
+    @GetMapping ("/select")
+    public List<Map<String, ?>> getMessage() {
+        return smd.selectAll();
+    }
+
+}
