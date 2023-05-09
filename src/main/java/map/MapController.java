1440bbd5f2b6751bf680e013428c72c80c18f32c
mingi1123 <min_gi1123@naver.com>
2023-05-09 23:52:21 +0900

    backup2

--- src/main/java/map/MapController.java
+++ src/main/java/map/MapController.java
@@ -1,7 +1,9 @@
-package SSUpower.demo;
+package map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -11,9 +13,8 @@ public class MapController {
     @Autowired
     MapDAO smd;
 
-    @GetMapping ("/select")
+    @GetMapping("/select")
     public List<Map<String, ?>> getMessage() {
         return smd.selectAll();
     }
-
 }
--- src/main/java/map/MapDAO.java
+++ src/main/java/map/MapDAO.java
@@ -1,4 +1,4 @@
-package SSUpower.demo;
+package map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
