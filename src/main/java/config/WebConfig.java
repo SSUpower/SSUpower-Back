0156806dce908967bd8d536c22da926ff3a11526
모험가 <80528044+mingi1123@users.noreply.github.com>
2023-05-16 18:34:18 +0900

    Update: exposedHeaders

--- src/main/java/config/WebConfig.java
+++ src/main/java/config/WebConfig.java
@@ -12,6 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:3000")
                 .allowedMethods("*")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(true)
+                .exposedHeaders("Access-Control-Allow-Origin"); 
     }
 }
