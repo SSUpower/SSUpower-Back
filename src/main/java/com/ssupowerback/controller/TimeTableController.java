ea04f8785d60d638fe5ead1b9b98732dd4b091b8
oxdjww <oxdjww@gmail.com>
2023-05-19 22:08:35 +0900

    Back repo merge

---
+++ src/main/java/com/ssupowerback/controller/TimeTableController.java
@@ -0,0 +1,30 @@
+package com.ssupowerback.controller;
+
+import com.ssupowerback.entity.TimeTableDAO;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+@RestController
+@RequestMapping("/timetable")
+@CrossOrigin(origins = "http://localhost:3000")
+
+
+public class TimeTableController {
+
+    private final TimeTableDAO timetableDAO;
+
+    public TimeTableController(TimeTableDAO timetableDAO) {
+        this.timetableDAO = timetableDAO;
+    }
+    @GetMapping("/select")
+    public List<Map<String, ?>> getTimeTable() {
+        return timetableDAO.selectTimeTable();
+    }
+
+    @PostMapping("/insert")
+    public List<Map<String, ?>> setTimeTable(@RequestBody Map<String, String> Submit) {
+        timetableDAO.insertTimeTable(Submit);
+        return timetableDAO.selectTimeTable();
+    }
+}
