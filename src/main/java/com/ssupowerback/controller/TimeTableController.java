a56d1177e2c2ccab52beed655812cbc69bed8057
byeoneunyoung <ebyeon147@soongsil.ac.kr>
2023-05-22 21:58:14 +0900

    building db fix4

--- src/main/java/com/ssupowerback/controller/TimeTableController.java
+++ src/main/java/com/ssupowerback/controller/TimeTableController.java
@@ -17,8 +17,8 @@ public class TimeTableController {
     public TimeTableController(TimeTableDAO timetableDAO) {
         this.timetableDAO = timetableDAO;
     }
-    @GetMapping("/{mId}/select")
-    public List<Map<String, ?>> getTimeTable(@PathVariable String mId) {
+    @PostMapping("/{mId}/select")
+    public List<Map<String, ?>> getTimeTable(@PathVariable @RequestBody String mId) {
         return timetableDAO.selectTimeTable(mId);
     }
 
