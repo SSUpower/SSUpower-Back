3a345b1b0b795a3f753bf7291655ffcc65346248
mingi1123 <min_gi1123@naver.com>
2023-06-07 15:55:01 +0900

    fix: delete Origin domain in TimeTableController

--- src/main/java/com/ssupowerback/controller/TimeTableController.java
+++ src/main/java/com/ssupowerback/controller/TimeTableController.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.Map;
 @RestController
 @RequestMapping("/timetable")
-@CrossOrigin(origins = "http://localhost:3000")
 
 
 public class TimeTableController {
